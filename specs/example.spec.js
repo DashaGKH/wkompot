d0ea9e70f61bf3c0cf0666fb37a91687800952de
DashaGKH <keshonti2016@gmail.com>
2021-11-22 17:33:24 -0500

    add auth test

--- specs/example.spec.js
+++ specs/example.spec.js
@@ -1,9 +1,11 @@
-import LoginPage from  '../../../pages/login.page';
-import SecurePage from '../../../pages/secure.page';
+import LoginPage from '../pages/login.page';
+import SecurePage from '../pages/secure.page';
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
         await LoginPage.open();
+
+        //DTest@exmpl.com
 
         await LoginPage.login('tomsmith', 'SuperSecretPassword!');
         await expect(SecurePage.flashAlert).toBeExisting();
