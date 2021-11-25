5a5a2687cc3d2b79d4b4b8fe1403c91a568c18d8
DashaGKH <keshonti2016@gmail.com>
2021-11-24 19:17:38 -0500

    add negative tests for auth spec

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -13,6 +13,23 @@ describe('Auth', function (){
         await expect(ProfilePage.iconUser)
             .toBeDisplayed();
 
+    });
 
+    it('Log in attempt with non-registered email', async function (){
+        await LoginPage.login('DTestinvalid@exmpl.com', 'Test1234');
+        await expect(LoginPage.notification).toHaveText('Email is not registered');
+    });
+    it('Log in attempt with invalid password', async function (){
+        await LoginPage.login('DTest@exmpl.com', 'Test1234invalid');
+        await expect(LoginPage.notification).toHaveText('Incorrect password');
+    });
+
+    it.only('Credentials are required', async function (){
+        await LoginPage.inputUsername.setValue('test')
+        await LoginPage.inputUsername.smartClear();
+        await expect(LoginPage.loginError).toHaveText('Required');
+        await LoginPage.inputPassword.setValue('test')
+        await LoginPage.inputPassword.smartClear();
+        await expect(LoginPage.passwordError).toHaveText('Required');
     });
  });
--- wdio.conf.js
+++ wdio.conf.js
@@ -26,4 +26,14 @@ exports.config = {
         ui: 'bdd',
         timeout: 60000
     },
+
+    before: async function(capabilities, specs,browser){
+        browser.addCommand('smartClear', async function () {
+            const text = await this.getValue();
+            for(let char of text){
+                this.keys('Backspace');
+            }
+            await this.click()
+        }, true);
+    },
 }
